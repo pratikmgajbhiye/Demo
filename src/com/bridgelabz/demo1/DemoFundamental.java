e8cc87a7f1655aed7a0a76c0774a4fe046d8a09d
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-04 18:03:13 +0530

    Added:Welcom to Java Fundamentals

---
+++ src/com/bridgelabz/demo1/DemoFundamental.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.demo1;
+
+public class DemoFundamental {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcom Java Fundamentals");
+	}
+
+}
